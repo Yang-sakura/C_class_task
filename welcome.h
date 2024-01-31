b9fb1216eaa5cb216d81ee207c8c6a86cdfeba5b
Yang-sakura <147797487+Yang-sakura@users.noreply.github.com>
2024-01-31 21:52:05 +0800

    Create welcome.h

---
+++ welcome.h
@@ -0,0 +1,11 @@
+#ifndef _WELCOME_H_
+#define _WELCOME_H_
+
+void welcome_screen(void);
+void put_title(int *color);
+
+int welcome_page(void);
+void welcome_buttons_recovery(int num);
+void welcome_buttons_light(int flag);
+
+#endif
