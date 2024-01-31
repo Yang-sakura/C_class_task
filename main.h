2bc458f66bdd35d85b2cf6ae9fc04e3e619ebd7b
Yang-sakura <147797487+Yang-sakura@users.noreply.github.com>
2024-01-31 16:01:21 +0800

    Create main.h

---
+++ main.h
@@ -0,0 +1,19 @@
+#ifndef _MAIN_H
+#define _MAIN_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <conio.h>
+#include <graphics.h>
+#include <string.h>
+
+#define WELCOME 0
+#define SIGH_IN 1
+#define SIGH_UP 2
+#define HOME 3
+#define FIELD 4
+#define DRONE 5
+#define PESTICIDE 6
+#define DETECTOR 7
+
+#endif
