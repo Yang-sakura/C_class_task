20c8f7cb904c935d016a6c99a03a07f54f7cb381
xuznotfound <141018245+xuznotfound@users.noreply.github.com>
2024-01-31 22:30:06 +0800

    Create login.h

---
+++ login.h
@@ -0,0 +1,10 @@
+#ifndef _LOGIN_H_
+#define _LOGIN_H_
+
+int login_page(void);
+void login_bkpaint(void);//画登录页面背景
+void back_button_light(void);
+void buttons_recover(void);
+void signup_button_light(void);
+
+#endif
