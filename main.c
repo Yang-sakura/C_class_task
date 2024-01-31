a2df1bf7bde936c7927272eb35cd6c8cddee1111
Yang-sakura <147797487+Yang-sakura@users.noreply.github.com>
2024-01-31 16:00:27 +0800

    Update main.c

--- main.c
+++ main.c
@@ -1,2 +1,52 @@
-The body of the task
-#include<stdio.h>
+#include "main.h"
+
+void main()
+{
+    int gdriver = VGA;
+    int gmode = VGAHI;
+    int page = 0;
+    initgraph(&gdriver,&gmode,"c:\\borlandc\\bgi");
+    while(1)
+    {
+        switch(page)
+        {
+            case WELCOME :
+            page = 1; 
+            break;
+
+            case SIGH_IN :
+            page = 1;
+            break;
+
+            case SIGH_UP :
+            page = 1;
+            break;
+
+            case HOME :
+            page = 1;
+            break;
+
+            case FIELD :
+            page = 1;
+            break;
+
+            case DRONE :
+            page = 1;
+            break;
+
+            case PESTICIDE :
+            page = 1;
+            break;
+
+            case DETECTOR :
+            page = 1;
+            break; 
+        }
+        getch();
+        closegraph();
+    }
+
+    getch();
+    closegraph();
+
+}
