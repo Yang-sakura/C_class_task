8acb79ade9995bc14cb10807e83b61ca407cfb32
Yang-sakura <147797487+Yang-sakura@users.noreply.github.com>
2024-02-01 00:37:08 +0800

    Update welcome.h

--- welcome.h
+++ welcome.h
@@ -3,7 +3,8 @@
 
 void welcome_screen(void);
 void put_title(int *color);
-
+void drone(void);
+void drone_wing(int* drone_flag,int x,int y);
 int welcome_page(void);
 void welcome_buttons_recovery(int num);
 void welcome_buttons_light(int flag);
