b93d0f7a30e4e0afc8e9d00a90bcfbdb893e718f
xuznotfound <141018245+xuznotfound@users.noreply.github.com>
2024-01-31 20:49:49 +0800

    Update main.c

--- main.c
+++ main.c
@@ -55,11 +55,12 @@ void main()
 function:printline
 description:画像素风线函数
 create:24/1/31
-input:int x,int y,int len,int n,int flag,int COLOR
+input:int x,int y,int len,int n,int flag,int COLOR,int wid,int gap
       x,y是第一个小方块左上角起始点，flag=0横着向右，flag=1竖着向左，len记录每个块有多长，n记录有多少格
+      wid每格宽度，每格长度为len倍的宽度,gap为每格之间间隙
 output:void
 ************************* */
-void printline(int x,int y,int len,int n,int flag,int COLOR)
+void printline(int x,int y,int len,int n,int flag,int COLOR,int wid,int gap)
 {
    //wid每格宽度，每格长度为len倍的宽度,gap为每格之间间隙
    int t;
@@ -77,5 +78,22 @@ void printline(int x,int y,int len,int n,int flag,int COLOR)
       if(flag==0) x+=gap;
       else y+=gap;
    }
+
+}
+
+void printbox(int x1,int y1,int x2,int y2,int COLOR,int len,int wid,int gap)
+{
+   int ver,hor,sum,remain_ver,remain_hor;//vertical,horizontal
+   sum=wid*len+gap;
+   hor=abs(x2-x1)/sum;
+   remain_hor=abs(x2-x1)-(hor*sum-gap);
+   if(remain_hor<wid*len) remain_hor+=wid*len;
+   ver=abs(y2-y1)/sum;
+   remain_ver=abs(y2-y1)-(ver*sum-gap);
+   if(remain_ver<wid*len) remain_ver+=wid*len;
+   printline(x1+remain_hor/2,y1    ,len,hor,0,COLOR,wid,gap);
+   printline(x1+remain_hor/2,y2-wid,len,hor,0,COLOR,wid,gap);
+   printline(x1,y1+remain_ver/2,len,ver,1,COLOR,wid,gap);
+   printline(x2-wid,y1+remain_ver/2,len,ver,1,COLOR,wid,gap);
 
 }
