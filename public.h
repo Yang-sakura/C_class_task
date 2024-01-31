5d40cb212e40e1d6f3e064406a1c172fbc3cbd7b
xuznotfound <141018245+xuznotfound@users.noreply.github.com>
2024-01-31 19:57:08 +0800

    Update public.h

--- public.h
+++ public.h
@@ -57,11 +57,11 @@
 // 	char user[15];	   //用户名
 // 	char password[20]; //密码
 // } INFO;
-extern int wid;
-extern int gap;
+#define wid 5
+#define gap 5
 
-void printline(int x,int y,int len,int flag,int COLOR);
-//x,y是第一个小方块左上角起始点，flag=0横着向右，flag=1竖着向左，len记录画多少个,COLOR是画线颜色
+void printline(int x,int y,int len,int n,int flag,int COLOR);
+//x,y是第一个小方块左上角起始点，flag=0横着向右，flag=1竖着向左，len每格长度是宽度的多少倍数，n记录画多少个,COLOR是画线颜色
 
 
 #endif
