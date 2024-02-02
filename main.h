eda47cc19704f01afff1e1cfc5fe5e60810aae8a
xuznotfound <141018245+xuznotfound@users.noreply.github.com>
2024-02-02 17:44:17 +0800

    Update main.h

--- main.h
+++ main.h
@@ -1,15 +1,13 @@
 #ifndef _MAIN_H
 #define _MAIN_H
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <conio.h>
-#include <graphics.h>
-#include <string.h>
+#include "login.h"
+#include "welcome.h"
+#include "signup.h"
 
 #define WELCOME 0
 #define LOGIN 1
-#define SIGH_UP 2
+#define SIGHUP 2
 #define HOME 3
 #define FIELD 4
 #define DRONE 5
