703a466df7fd29998e32d21b935185336522a324
xuznotfound <141018245+xuznotfound@users.noreply.github.com>
2024-02-05 19:41:03 +0800

    Update logfunc.h

--- logfunc.h
+++ logfunc.h
@@ -3,5 +3,12 @@
 
 int check(INFO *user);
 void temp_input(char *temp,int x,int y);
+int userinfo_input(INFO *user,int *state1,int *state2);
+void password_warning(char *s);
+void title_warning(char *s,int PAGE);
+int password_check(const char *password);
+int user_exist_check(const char *username);
+void creat_user_directory(INFO *user);
+void creat_userinfo_file(INFO *user);
 
 #endif
