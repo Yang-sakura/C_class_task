d112fd956037fa2f3a6dd99aee0b0ed5249ff3df
Yang-sakura <147797487+Yang-sakura@users.noreply.github.com>
2024-02-05 22:52:28 +0800

    Update fieldfunc.c

--- fieldfunc.c
+++ fieldfunc.c
@@ -27,45 +27,95 @@ void fieldfunc_screen()
     }
 
     back_button(PAINT);
+
+    put_pencil(10,50,DARKGRAY,DARKGRAY,LIGHTGRAY,5);
 }
 
-void fieldfunc_page()
+void fieldfunc_page(char *name)
 {
     int record[21][26];
+    int mode = 0;
     int flag = 0;
     int (*precord)[26] = record;
     int i;
     FILE *fp;
+    char string[80] = "c:\\DATA\\";
+    char filename[80];
+    strcat(string,name);
+    strcat(string,"\\FIELD");
+    if(access(string,0)==-1)
+    {
+        if(mkdir(string)!=0)
+        {
+            perror("ERROR IN BUILDING THE FIELD PACKAGE!");
+            delay(2000);
+            exit(1);
+        }
+    }
+
+
+
     clrmous(MouseX,MouseY);
     cleardevice();
     fieldfunc_screen();
+    mouseinit();
     while(1)
     {
         newmouse(&MouseX,&MouseY,&press);
-        if(mouse_press(110,50,630,470)==1)
-        {
-            clrmous(MouseX,MouseY);
-            setfillstyle(SOLID_FILL,DARKGRAY);
-            bar(110+20*((MouseX - 110)/20),470-20*((470-MouseY)/20+1),110+20*((MouseX - 110)/20)+20,470-20*((470-MouseY)/20+1)+20);
-            record[(MouseX - 110)/20][(470-MouseY)/20] = 1;
-            
 
-        }
-        if((fp = fopen("c:\\code\\source\\file\\field.dat","wb"))!=NULL)
+        if(mouse_press(10,50,75,115)==2)         //铅笔不点击
+        {
+            if(flag!=1)
             {
-                for(i=0;i<21;i++)
-                {
-                    fwrite(precord[i],sizeof(int),26,fp);
-                }
+                MouseS = 1;
+                flag = 1;
+                clrmous(MouseX,MouseY);
+                put_pencil(10,50,DARKGRAY,BROWN,LIGHTGRAY,5);
             }
-            fclose(fp);
+        }
+        else if(mouse_press(10,50,75,115)==1)     //mode1代表画图模式
+        {
+            mode = 1;
+            MouseS = mode+3;
+            clrmous(MouseX,MouseY);
+            put_pencil(10,50,DARKGRAY,BROWN,LIGHTGRAY,5);
+            printbox(5,350,80,470,DARKGRAY,1,5,5);
+            settextstyle(DEFAULT_FONT,HORIZ_DIR,3);
+            outtextxy(400,10,"OK");
+        
+        }
 
         if(mouse_press(595,5,630,40)==1)
         exit(1);
         
-        
-        
-        
+        if(mode ==1)
+        {
+            while(1)
+            {
+                newmouse(&MouseX,&MouseY,&press);
+                if(mouse_press(110,50,630,470)==1)
+                {
+                    clrmous(MouseX,MouseY);
+                    setfillstyle(SOLID_FILL,DARKGRAY);
+                    bar(110+20*((MouseX - 110)/20),470-20*((470-MouseY)/20+1),110+20*((MouseX - 110)/20)+20,470-20*((470-MouseY)/20+1)+20);
+                    record[(MouseX - 110)/20][(470-MouseY)/20] = 1;               
+                }
+                else if(mouse_press(5,350,80,470)==1)
+                {
+                    MouseS = 1;
+                    mode = 0;
+                    break;
+                }
+                if((fp = fopen("c:\\DATA\\yang\\FIELD\\field.dat","wb"))!=NULL)
+                {
+                    for(i=0;i<21;i++)
+                    {
+                        fwrite(precord[i],sizeof(int),26,fp);
+                    }
+                }
+                fclose(fp);
+            }
+        }
+         
     }
 }
-
