8d6d12df9de0fe874060e9333141ce6d60f94e2c
xuznotfound <141018245+xuznotfound@users.noreply.github.com>
2024-02-06 13:02:26 +0800

    Update draw.c

--- draw.c
+++ draw.c
@@ -85,3 +85,49 @@ void put_house(int x,int y,int DARKCOLOR,int LIGHTCOLOR,int pix)
 
 }
 
+void put_flower(int x,int y,int pix,int COLOR)
+{
+    // void printline(int x,int y,int len,int n,int flag,int COLOR,int wid,int gap);
+    printline(x,y-pix*9,1,9,1,COLOR,pix,0);
+    printline(x-2*pix,y-pix*6,1,5,0,COLOR,pix,0);
+    printline(x-2*pix,y-pix*5,1,5,0,COLOR,pix,0);
+    printline(x-2*pix,y-pix*10,1,4,1,COLOR,pix,0);
+    printline(x-3*pix,y-pix*9,1,3,1,COLOR,pix,0);
+    printline(x+2*pix,y-pix*10,1,4,1,COLOR,pix,0);
+    printline(x+3*pix,y-pix*9,1,3,1,COLOR,pix,0);
+    printline(x,y-pix*8,1,3,0,COLOR,pix,0);
+    printline(x-4*pix,y-pix*4,1,2,1,COLOR,pix,0);
+    printline(x-3*pix,y-pix*3,1,2,1,COLOR,pix,0);
+    printline(x+4*pix,y-pix*4,1,2,1,COLOR,pix,0);
+    printline(x+3*pix,y-pix*3,1,2,1,COLOR,pix,0);
+    printline(x-2*pix,y-pix*2,1,5,0,COLOR,pix,0);
+    printline(x-2*pix,y-pix*1,1,5,0,COLOR,pix,0);
+    printline(x-pix,y-pix*7,1,3,1,COLOR,pix,0);
+}
+
+void put_shovel(int x,int y)
+{
+    setcolor(DARKGRAY);
+    setlinestyle(SOLID_LINE,0,NORM_WIDTH);
+    line(x-1,y-2,x+2,y-2);
+    line(x-1,y-1,x+3,y-1);
+    line(x-1,y,x+4,y);
+    line(x-1,y+1,x+3,y+1);
+    line(x,y+2,x+2,y+2);
+    putpixel(x+1,y+3,DARKGRAY);
+
+    setcolor(BROWN);
+    line(x+3,y+2,x+4,y+2);
+
+    line(x+3,y+3,x+5,y+3);//x-1 y-2
+    line(x+4,y+4,x+6,y+4);
+    line(x+5,y+5,x+7,y+5);
+    line(x+6,y+6,x+8,y+6);
+    line(x+7,y+7,x+9,y+7);
+
+    line(x+8,y+8,x+11,y+8);
+    line(x+9,y+9,x+10,y+9);
+    line(x+8,y+10,x+9,y+10);
+    putpixel(x+11,y+7,BROWN);
+
+}
