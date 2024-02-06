61587a63d8d470a7eff3feb4b4fae1a5e8bb11ec
Yang-sakura <147797487+Yang-sakura@users.noreply.github.com>
2024-02-06 13:56:04 +0800

    Update fieldfunc.h

--- fieldfunc.h
+++ fieldfunc.h
@@ -1,8 +1,8 @@
 #ifndef _FIELDFUNC_H_
 #define _FIELDFUNC_H_
 
-void fieldfunc_screen();
-void fieldfunc_page(char *name);
+void draw_field_screen();
+int draw_field_page(char *name);
 void temp_input(char *temp,int x,int y);
 void put_ok_button(int flag);
 #endif
