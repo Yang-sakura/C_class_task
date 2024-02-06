10f9e2ea393bb2065a198fd7bc0cff688a303d69
Yang-sakura <147797487+Yang-sakura@users.noreply.github.com>
2024-02-06 12:51:22 +0800

    Create draw.h

---
+++ draw.h
@@ -0,0 +1,10 @@
+#ifndef _DRAW_H_
+#define _DRAW_H_
+
+void put_pencil(int x,int y,int COLOR1,int COLOR2,int COLOR3,int pix);
+void put_rubber(int x,int y,int COLOR,int pix);
+void put_sprout(int x,int y,int COLOR,int pix);
+void put_field(int x,int y,int LIGHTCOLOR,int DARKCOLOR,int pix);
+void put_house(int x,int y,int DARKCOLOR,int LIGHTCOLOR,int pix);
+
+#endif
