4374c82779333ed1428e8128c61baa2a911117fc
Yang-sakura <147797487+Yang-sakura@users.noreply.github.com>
2024-02-06 12:49:51 +0800

    Update fieldfunc.c

--- fieldfunc.c
+++ fieldfunc.c
@@ -1,41 +1,22 @@
 #include "fieldfunc.h"
 #include "public.h"
+#include "main.h"
 
 void fieldfunc_screen()
 {
-    int i;
-    printline(100,0,1,480,1,DARKGRAY,5,5);
-    setcolor(DARKGRAY);
-    settextstyle(DEFAULT_FONT,HORIZ_DIR,4);
-    outtextxy(110,10,"NAME:");
-
-    setlinestyle(SOLID_LINE,0,THICK_WIDTH);
-    line(110,50,110,470);
-    line(110,470,630,470);
-    line(110,50,108,60);
-    line(110,50,112,60);
-    line(630,470,620,468);
-    line(630,470,620,472);
-    setlinestyle(DOTTED_LINE,0,NORM_WIDTH);
-    for(i=0;i<26;i++)
-    {
-        line(110+i*20,50,110+i*20,470);
-    }
-    for(i=0;i<21;i++)
-    {
-        line(110,470-i*20,630,470-i*20);
-    }
-
     back_button(PAINT);
 
-    put_pencil(10,50,DARKGRAY,DARKGRAY,LIGHTGRAY,5);
+    put_pencil(12,50,DARKGRAY,DARKGRAY,LIGHTGRAY,5);
+    put_rubber(12,150,DARKGRAY,5);
 }
 
 void fieldfunc_page(char *name)
 {
     int record[21][26];
     int mode = 0;
+    int num[5];
     int flag = 0;
+    int pencil_flag = 0,rubber_flag = 0;
     int (*precord)[26] = record;
     int i;
     FILE *fp;
@@ -43,7 +24,7 @@ void fieldfunc_page(char *name)
     char filename[80];
     strcat(string,name);
     strcat(string,"\\FIELD");
-    if(access(string,0)==-1)
+    if(access(string,0)==-1)  //协助创建农田文件夹
     {
         if(mkdir(string)!=0)
         {
@@ -53,69 +34,268 @@ void fieldfunc_page(char *name)
         }
     }
 
-
-
     clrmous(MouseX,MouseY);
-    cleardevice();
+    setfillstyle(SOLID_FILL,WHITE);
+    bar(0,0,95,480);
     fieldfunc_screen();
     mouseinit();
     while(1)
     {
+        
         newmouse(&MouseX,&MouseY,&press);
 
-        if(mouse_press(10,50,75,115)==2)         //铅笔不点击
+        if(mouse_press(12,50,77,115)==2)         //铅笔不点击
         {
             if(flag!=1)
             {
                 MouseS = 1;
                 flag = 1;
+                num[1] = 1;
                 clrmous(MouseX,MouseY);
-                put_pencil(10,50,DARKGRAY,BROWN,LIGHTGRAY,5);
+                put_pencil(12,50,DARKGRAY,BROWN,LIGHTGRAY,5);
             }
         }
-        else if(mouse_press(10,50,75,115)==1)     //mode1代表画图模式
+        else if(mouse_press(12,50,77,115)==1)      //铅笔点击
         {
             mode = 1;
-            MouseS = mode+3;
+            MouseS = 0;
             clrmous(MouseX,MouseY);
-            put_pencil(10,50,DARKGRAY,BROWN,LIGHTGRAY,5);
-            printbox(5,350,80,470,DARKGRAY,1,5,5);
-            settextstyle(DEFAULT_FONT,HORIZ_DIR,3);
-            outtextxy(400,10,"OK");
-        
+        }
+        else if(mouse_press(12,150,77,215)==2)     //橡皮不点击
+        {
+            if(flag!=2)
+            {
+                MouseS = 1;
+                flag = 2;
+                num[2] =1;
+                clrmous(MouseX,MouseY);
+                put_rubber(12,150,LIGHTGRAY,5);
+            }
+        } 
+        else if(mouse_press(12,150,77,215)==1)       //橡皮点击
+        {
+            mode = 2;
+            MouseS = 0;
+            clrmous(MouseX,MouseY);
+        }
+        else if(mouse_press(595,5,630,40)==2)        //退出未点击
+        {
+            if(flag!=4)
+            {
+                MouseS = 1;
+                flag = 4;
+                num[4] =1;
+                clrmous(MouseX,MouseY);
+                back_button(LIGHT);
+            }
+        } 
+        else if(mouse_press(595,5,630,40)==1)        //退出点击
+        {
+            clrmous(MouseX,MouseY);
+            setcolor(WHITE);
+            setfillstyle(SOLID_FILL,WHITE);
+            bar(0,0,95,480);
+            field_page(name);
+        }
+        else
+        {
+            if(flag!=5)
+            {
+                MouseS = 0;
+                flag = 5;
+            }
         }
 
-        if(mouse_press(595,5,630,40)==1)
-        exit(1);
-        
-        if(mode ==1)
+
+        if(flag!=1&&num[1]==1)
         {
+            clrmous(MouseX,MouseY);
+            put_pencil(12,50,DARKGRAY,DARKGRAY,LIGHTGRAY,5);
+            num[1] = 0;
+        }
+        else if(flag!=2&&num[2]==1)
+        {
+            clrmous(MouseX,MouseY);
+            put_rubber(12,150,DARKGRAY,5);
+            num[2] = 0;
+        }
+        else if(flag!=4&&num[4]==1)
+        {
+            clrmous(MouseX,MouseY);
+            back_button(RECOVER);
+            num[4] = 0;
+        }
+
+
+        if(mode ==1)   //mode1代表画图模式
+        {
+            put_pencil(12,50,DARKGRAY,BROWN,LIGHTGRAY,5);
+            put_ok_button(PAINT);
+            setfillstyle(SOLID_FILL,WHITE);
+            bar(595,5,630,40);
             while(1)
             {
                 newmouse(&MouseX,&MouseY,&press);
-                if(mouse_press(110,50,630,470)==1)
+                if(mouse_press(110,50,630,470)==2) //处于画图区域，但未点击
+                {
+                    if(pencil_flag!=1)
+                    {
+                        MouseS = mode+3;
+                        pencil_flag = 1;
+                    }
+                }
+                else if(mouse_press(110,50,630,470)==1)      //处于画图区域并且点击
                 {
                     clrmous(MouseX,MouseY);
                     setfillstyle(SOLID_FILL,DARKGRAY);
                     bar(110+20*((MouseX - 110)/20),470-20*((470-MouseY)/20+1),110+20*((MouseX - 110)/20)+20,470-20*((470-MouseY)/20+1)+20);
                     record[(MouseX - 110)/20][(470-MouseY)/20] = 1;               
                 }
-                else if(mouse_press(5,350,80,470)==1)
+                else if(mouse_press(5,400,95,470)==2)      //处于ok区域未点击
                 {
-                    MouseS = 1;
-                    mode = 0;
-                    break;
-                }
-                if((fp = fopen("c:\\DATA\\yang\\FIELD\\field.dat","wb"))!=NULL)
-                {
-                    for(i=0;i<21;i++)
+                    if(pencil_flag!=2)
                     {
-                        fwrite(precord[i],sizeof(int),26,fp);
+                        MouseS = 1;
+                        pencil_flag = 2;
+                        put_ok_button(LIGHT);
                     }
                 }
-                fclose(fp);
+                else if(mouse_press(5,400,95,470)==1)        //处于ok区域并且点击
+                {
+                    MouseS = 0;
+                    mode = 0;
+
+                    if((fp = fopen("c:\\DATA\\yang\\FIELD\\field.dat","wb"))!=NULL)
+                    {
+                        for(i=0;i<21;i++)
+                        {
+                            fwrite(precord[i],sizeof(int),26,fp);
+                        }
+                    }
+                    else
+                    {
+                        perror("ERROR IN RECORDING!");
+                    }
+                    fclose(fp);
+
+                    clrmous(MouseX,MouseY);
+                    setfillstyle(SOLID_FILL,WHITE);
+                    bar(0,0,95,480);
+                    fieldfunc_page(name);
+                }
+                else 
+                {
+                    if(pencil_flag!=0)
+                    {
+                        MouseS = 0;
+                        pencil_flag = 0;
+                        put_ok_button(RECOVER);
+                    }
+                }
+            }
+        }
+
+        if(mode ==2)   //mode2代表擦除模式
+        {
+            put_rubber(12,150,LIGHTGRAY,5);
+            put_ok_button(PAINT);
+            setfillstyle(SOLID_FILL,WHITE);
+            bar(595,5,630,40);
+            while(1)
+            {
+                newmouse(&MouseX,&MouseY,&press);
+                if(mouse_press(110,50,630,470)==2) //处于画图区域，但未点击
+                {
+                    if(rubber_flag!=1)
+                    {
+                        MouseS = mode+3;
+                        rubber_flag = 1;
+                    }
+                }
+                else if(mouse_press(110,50,630,470)==1)      //处于画图区域并且点击
+                {
+                    clrmous(MouseX,MouseY);
+                    setfillstyle(SOLID_FILL,WHITE);
+                    bar(110+20*((MouseX - 110)/20),470-20*((470-MouseY)/20+1),110+20*((MouseX - 110)/20)+20,470-20*((470-MouseY)/20+1)+20);
+                    setcolor(DARKGRAY);
+                    setlinestyle(DOTTED_LINE,0,NORM_WIDTH);
+                    line(110+20*((MouseX - 110)/20),470-20*((470-MouseY)/20+1),110+20*((MouseX - 110)/20),470-20*((470-MouseY)/20+1)+20);
+                    line(110+20*((MouseX - 110)/20),470-20*((470-MouseY)/20+1),110+20*((MouseX - 110)/20)+20,470-20*((470-MouseY)/20+1));
+                    line(110+20*((MouseX - 110)/20),470-20*((470-MouseY)/20+1)+20,110+20*((MouseX - 110)/20)+20,470-20*((470-MouseY)/20+1)+20);
+                    line(110+20*((MouseX - 110)/20)+20,470-20*((470-MouseY)/20+1)+20,110+20*((MouseX - 110)/20)+20,470-20*((470-MouseY)/20+1));
+                    record[(MouseX - 110)/20][(470-MouseY)/20] = 0;               
+                }
+                else if(mouse_press(5,400,95,470)==2)      //处于ok区域未点击
+                {
+                    if(rubber_flag!=2)
+                    {
+                        MouseS = 1;
+                        rubber_flag = 2;
+                        put_ok_button(LIGHT);
+                    }
+                }
+                else if(mouse_press(5,400,95,470)==1)        //处于ok区域并且点击
+                {
+                    MouseS = 0;
+                    mode = 0;
+
+                    if((fp = fopen("c:\\DATA\\yang\\FIELD\\field.dat","wb"))!=NULL)
+                    {
+                        for(i=0;i<21;i++)
+                        {
+                            fwrite(precord[i],sizeof(int),26,fp);
+                        }
+                    }
+                    else
+                    {
+                        perror("ERROR IN RECORDING!");
+                    }
+                    fclose(fp);
+
+                    clrmous(MouseX,MouseY);
+                    setfillstyle(SOLID_FILL,WHITE);
+                    bar(0,0,95,480);
+                    fieldfunc_page(name);
+                }
+                else 
+                {
+                    if(rubber_flag!=0)
+                    {
+                        MouseS = 0;
+                        rubber_flag = 0;
+                        put_ok_button(RECOVER);
+                    }
+                }
             }
         }
          
     }
 }
+
+
+void put_ok_button(int flag)
+{
+    if(flag == PAINT)
+    {
+        clrmous(MouseX,MouseY);
+        printbox(5,400,95,470,DARKGRAY,1,5,5);
+        setcolor(DARKGRAY);
+        settextstyle(DEFAULT_FONT,HORIZ_DIR,3);
+        outtextxy(30,425,"OK");
+    }
+    else if(flag == LIGHT)
+    {
+        clrmous(MouseX,MouseY);
+        printbox(5,400,95,470,YELLOW,1,5,5);
+        setcolor(CYAN);
+        settextstyle(DEFAULT_FONT,HORIZ_DIR,3);
+        outtextxy(30,425,"OK");
+    }
+    else if(flag == RECOVER)
+    {
+        put_ok_button(PAINT);
+    }
+}
+
+
+
