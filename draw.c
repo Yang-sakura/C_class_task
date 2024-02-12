03aa1a76ff9dd9123ea72871f4977573d93acd1b
Yang-sakura <147797487+Yang-sakura@users.noreply.github.com>
2024-02-12 10:31:34 +0800

    Update draw.c

--- draw.c
+++ draw.c
@@ -100,25 +100,32 @@ void put_house(int x,int y,int DARKCOLOR,int LIGHTCOLOR,int pix)
 
 }
 
-void put_flower(int x,int y,int pix,int COLOR)
+void put_arrow(int x,int y,int COLOR,int pix,int flag)
 {
-    // void printline(int x,int y,int len,int n,int flag,int COLOR,int wid,int gap);
-    printline(x,y-pix*9,1,9,1,COLOR,pix,0);
-    printline(x-2*pix,y-pix*6,1,5,0,COLOR,pix,0);
-    printline(x-2*pix,y-pix*5,1,5,0,COLOR,pix,0);
-    printline(x-2*pix,y-pix*10,1,4,1,COLOR,pix,0);
-    printline(x-3*pix,y-pix*9,1,3,1,COLOR,pix,0);
-    printline(x+2*pix,y-pix*10,1,4,1,COLOR,pix,0);
-    printline(x+3*pix,y-pix*9,1,3,1,COLOR,pix,0);
-    printline(x,y-pix*8,1,3,0,COLOR,pix,0);
-    printline(x-4*pix,y-pix*4,1,2,1,COLOR,pix,0);
-    printline(x-3*pix,y-pix*3,1,2,1,COLOR,pix,0);
-    printline(x+4*pix,y-pix*4,1,2,1,COLOR,pix,0);
-    printline(x+3*pix,y-pix*3,1,2,1,COLOR,pix,0);
-    printline(x-2*pix,y-pix*2,1,5,0,COLOR,pix,0);
-    printline(x-2*pix,y-pix*1,1,5,0,COLOR,pix,0);
-    printline(x-pix,y-pix*7,1,3,1,COLOR,pix,0);
+    if(flag == LEFT) 
+    {
+        printline(x+3*pix,y,1,2,0,COLOR,pix,0);
+        printline(x+2*pix,y+pix,1,3,0,COLOR,pix,0);
+        printline(x+pix,y+2*pix,1,15,0,COLOR,pix,0);
+        printline(x,y+3*pix,1,16,0,COLOR,pix,0);
+        printline(x,y+4*pix,1,16,0,COLOR,pix,0);
+        printline(x+pix,y+5*pix,1,15,0,COLOR,pix,0);
+        printline(x+2*pix,y+6*pix,1,3,0,COLOR,pix,0);
+        printline(x+3*pix,y+7*pix,1,2,0,COLOR,pix,0);
+    }
+    else if(flag == RIGHT)
+    {
+        printline(x+11*pix,y,1,2,0,COLOR,pix,0);
+        printline(x+11*pix,y+pix,1,3,0,COLOR,pix,0);
+        printline(x,y+2*pix,1,15,0,COLOR,pix,0);
+        printline(x,y+3*pix,1,16,0,COLOR,pix,0);
+        printline(x,y+4*pix,1,16,0,COLOR,pix,0);
+        printline(x,y+5*pix,1,15,0,COLOR,pix,0);
+        printline(x+11*pix,y+6*pix,1,3,0,COLOR,pix,0);
+        printline(x+11*pix,y+7*pix,1,2,0,COLOR,pix,0);
+    }
 }
+
 void put_shovel(int x,int y,int pix,int COLOR1,int COLOR2)
 {
     printline(x,y,1,5,0,COLOR1,pix,0);
@@ -140,3 +147,4 @@ void put_shovel(int x,int y,int pix,int COLOR1,int COLOR2)
     printline(x+8*pix,y+12*pix,1,3,0,COLOR2,pix,0);
     printline(x+12*pix,y+9*pix,1,3,1,COLOR2,pix,0);
 }
+
