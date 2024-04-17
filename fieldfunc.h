2c84226a996a85d78da61f07d8490c827ff6992a
xuznotfound <141018245+xuznotfound@users.noreply.github.com>
2024-04-17 21:09:18 +0800

    Update fieldfunc.h

--- fieldfunc.h
+++ fieldfunc.h
@@ -1,9 +1,18 @@
-#ifndef _FIELDFUNC_H_
-#define _FIELDFUNC_H_
+#ifndef _FLYFUNC_H_
+#define _FLYFUNC_H_
 
-void draw_field_screen(void);
-int draw_field_page(char *name,char *now_field);
-void put_ok_button(int flag);
-void open_file(void);
+void save_bk_drone(int nx, int ny);
+void clear_drone(int nx, int ny) ;
+void draw_drone(int nx, int ny);
+
+int x_record_to_screen(int x);
+int y_record_to_screen(int y);
+void new_field( int record[21][26] ,char *nowfield);
+void update_field(int record[21][26], int drone_x , int drone_y );
+void move_drone1(int record[21][26], int x1,int y1,int x2,int y2 );
+
+void simulate( int record[21][26] ,char *nowfield);
+
+void simulate_handmode(int record[21][26] , int route[100][2] );
 
 #endif
