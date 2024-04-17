2adf1630a826f0a747a35ce3bfea71c29f6661b3
Yang-sakura <147797487+Yang-sakura@users.noreply.github.com>
2024-04-17 20:33:12 +0800

    Update public.h

--- public.h
+++ public.h
@@ -60,6 +60,13 @@
 #define LIGHT 2
 #define DELETE 3
 
+#define SPROUT 1
+#define TRANSITION 2
+#define CROP 3
+
+#define SICK 1
+#define HEALTHY 2
+
 void printline(int x,int y,int len,int n,int flag,int COLOR,int wid,int gap);
 //x,y是第一个小方块左上角起始点，flag=0横着向右，flag=1竖着向下，len每格长度是宽度的多少倍数，n记录画多少个,COLOR是画线颜色
 void printbox(int x1,int y1,int x2,int y2,int COLOR,int len,int wid,int gap);
