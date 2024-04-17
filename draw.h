44ffe5d45e76b8f29d871238ebaea2f3b9a5739a
Yang-sakura <147797487+Yang-sakura@users.noreply.github.com>
2024-04-17 20:32:38 +0800

    Update draw.h

--- draw.h
+++ draw.h
@@ -9,4 +9,7 @@ void put_house(int x,int y,int DARKCOLOR,int LIGHTCOLOR,int pix);
 void put_file(int x,int y,int DARKCOLOR,int LIGHTCOLOR,int pix);
 void put_arrow(int x,int y,int COLOR,int pix,int flag);
 void put_shovel(int x,int y,int pix,int COLOR1,int COLOR2);
+void put_crop1(int x,int y,int type,int situation);
+void put_crop2(int x,int y,int type,int situation);
+void put_crop3(int x,int y,int type,int situation);
 #endif
