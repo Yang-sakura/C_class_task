7a53d84453984577af1da64e0002534c658a66af
xuznotfound <141018245+xuznotfound@users.noreply.github.com>
2024-04-20 15:56:34 +0800

    Update detect.h

--- detect.h
+++ detect.h
@@ -6,9 +6,11 @@ void put_calender_number(char *date);
 void pause_button(int flag);
 void mode_button(int flag);
 void chart_button(int flag);
+void route_button(int flag);
 void paint_field( int record[21][26] ,char *nowfield);
 void start_button(int flag);
 void detect_screen(int record[21][26] , char *nowfield);
 int detect_page(char *username,char *nowfield);
+void grow(int record[21][26] , int date );
 
 #endif
