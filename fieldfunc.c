d3aff8ffdee5a227bb537ea198571015bdf3c690
Yang-sakura <147797487+Yang-sakura@users.noreply.github.com>
2024-04-23 07:07:18 +0800

    Update fieldfunc.c

--- fieldfunc.c
+++ fieldfunc.c
@@ -31,7 +31,9 @@ void draw_field_screen()
 
     put_pencil(12,50,DARKGRAY,DARKGRAY,LIGHTGRAY,5);
     put_rubber(12,150,DARKGRAY,5);
-    put_file(12,260,DARKGRAY,LIGHTGRAY,5);
+    put_file(12,340,DARKGRAY,LIGHTGRAY,5);  //260
+    put_water(12,230,DARKGRAY,5);
+    
 }
 
 void open_file()
@@ -68,7 +70,7 @@ int draw_field_page(char *name,char *now_field)
     int num[5];
     int filenum[5];
     int flag = 0;
-    int pencil_flag = 0,rubber_flag = 0,file_flag = 0;
+    int pencil_flag = 0,rubber_flag = 0,file_flag = 0,water_flag=0;
     int (*precord)[26] = record;
     int i=0;
     int page=0,pagemax=0;
@@ -160,7 +162,24 @@ int draw_field_page(char *name,char *now_field)
             MouseS = 0;
             clrmous(MouseX,MouseY);
         }
-        else if(mouse_press(12,260,77,310)==2)       //文件夹未点击
+        else if(mouse_press(22,250,62,305)==2)
+        {
+            if(flag!=5)
+            {
+                MouseS = 1;
+                flag = 5;
+                num[5]=1;
+                clrmous(MouseX,MouseY);
+                put_water(12,230,BLUE,5);
+            }
+        }
+        else if(mouse_press(22,250,62,305)==1)
+        {
+            mode = 4;
+            MouseS = 0;
+            clrmous(MouseX,MouseY);
+        }
+        else if(mouse_press(12,340,77,390)==2)       //文件夹未点击
         {
             if(flag!=3)
             {
@@ -168,10 +187,10 @@ int draw_field_page(char *name,char *now_field)
                 flag = 3;
                 num[3]=1;
                 clrmous(MouseX,MouseY);
-                put_file(12,260,BLUE,LIGHTBLUE,5);
+                put_file(12,340,BLUE,LIGHTBLUE,5);
             }
         }
-        else if(mouse_press(12,260,77,310)==1)     //文件夹点击
+        else if(mouse_press(12,340,77,390)==1)     //文件夹点击
         {
             mode =3;
             MouseS = 0;
@@ -195,10 +214,10 @@ int draw_field_page(char *name,char *now_field)
         }
         else
         {
-            if(flag!=5)
+            if(flag!=0)
             {
                 MouseS = 0;
-                flag = 5;
+                flag = 0;
             }
         }
 
@@ -218,7 +237,7 @@ int draw_field_page(char *name,char *now_field)
         else if(flag!=3&&num[3]==1)
         {
             clrmous(MouseX,MouseY);
-            put_file(12,260,DARKGRAY,LIGHTGRAY,5);
+            put_file(12,340,DARKGRAY,LIGHTGRAY,5);
             num[3] = 0;
         }
         else if(flag!=4&&num[4]==1)
@@ -226,6 +245,12 @@ int draw_field_page(char *name,char *now_field)
             clrmous(MouseX,MouseY);
             back_button(RECOVER);
             num[4] = 0;
+        }
+        else if(flag!=5&&num[5]==1)
+        {
+            clrmous(MouseX,MouseY);
+            put_water(12,230,DARKGRAY,5);
+            num[5] = 0;
         }
 
 
@@ -384,7 +409,7 @@ int draw_field_page(char *name,char *now_field)
             open_file();
             put_arrow(120,353,DARKGRAY,5,1);
             put_arrow(540,353,DARKGRAY,5,2);
-            put_file(12,260,BLUE,LIGHTBLUE,5);
+            put_file(12,340,BLUE,LIGHTBLUE,5);
             setfillstyle(SOLID_FILL,WHITE);
             bar(595,5,630,40);
             strcpy(stringall,string);
@@ -619,7 +644,7 @@ int draw_field_page(char *name,char *now_field)
                     mode = 0;
                     return DRAW_FIELD;
                 }
-                else if(mouse_press(12,260,77,310)==2)       //文件夹未点击
+                else if(mouse_press(12,340,77,390)==2)       //文件夹未点击
                 {
                     if(file_flag!=9)
                     {
@@ -627,10 +652,10 @@ int draw_field_page(char *name,char *now_field)
                         file_flag = 9;
                         filenum[9]=1;
                         clrmous(MouseX,MouseY);
-                        put_file(12,260,DARKGRAY,LIGHTGRAY,5);
+                        put_file(12,340,DARKGRAY,LIGHTGRAY,5);
                     }
                 }
-                else if(mouse_press(12,260,77,310)==1)       //文件夹点击
+                else if(mouse_press(12,340,77,390)==1)       //文件夹点击
                 {
                     clrmous(MouseX,MouseY);
                     cleardevice();
@@ -715,6 +740,78 @@ int draw_field_page(char *name,char *now_field)
                 }
                 
             }
+        }
+
+        if(mode ==4)   //mode4代表水源模式
+        {
+            put_water(12,230,DARKGRAY,5);
+            put_ok_button(PAINT);
+            setfillstyle(SOLID_FILL,WHITE);
+            bar(595,5,630,40);
+            while(1)
+            {
+                newmouse(&MouseX,&MouseY,&press);
+                if(mouse_press(110,50,630,470)==2) //处于画图区域，但未点击
+                {
+                    if(water_flag!=1)
+                    {
+                        MouseS = mode+4;
+                        water_flag = 1;
+                    }
+                }
+                else if(mouse_press(110,50,630,470)==1)      //处于画图区域并且点击
+                {
+                    clrmous(MouseX,MouseY);
+                    setfillstyle(SOLID_FILL,LIGHTBLUE);
+                    bar(110+20*((MouseX - 110)/20),470-20*((470-MouseY)/20+1),110+20*((MouseX - 110)/20)+20,470-20*((470-MouseY)/20+1)+20);
+                    record[(470-MouseY)/20][(MouseX - 110)/20] = 2;               
+                }
+                else if(mouse_press(5,400,95,470)==2)      //处于ok区域未点击
+                {
+                    if(water_flag!=2)
+                    {
+                        MouseS = 1;
+                        water_flag = 2;
+                        put_ok_button(LIGHT);
+                    }
+                }
+                else if(mouse_press(5,400,95,470)==1)        //处于ok区域并且点击
+                {
+                    MouseS = 0;
+                    mode = 0;
+
+                    strcpy(stringnow,string);
+                    strcat(stringnow,"\\");
+                    strcat(stringnow,now_field);
+                    if((fp = fopen(stringnow,"wb"))!=NULL)
+                    {
+                        for(i=0;i<21;i++)
+                        {
+                            fwrite(precord[i],sizeof(int),26,fp);
+                        }
+                    }
+                    else
+                    {
+                        perror("ERROR IN RECORDING!");
+                    }
+                    fclose(fp);
+
+                    clrmous(MouseX,MouseY);
+                    setfillstyle(SOLID_FILL,WHITE);
+                    bar(0,0,95,480);
+                    break;
+                }
+                else 
+                {
+                    if(pencil_flag!=0)
+                    {
+                        MouseS = 0;
+                        pencil_flag = 0;
+                        put_ok_button(RECOVER);
+                    }
+                }
+            }
+            return DRAW_FIELD;
         }
          
     }
