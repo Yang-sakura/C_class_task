2b671fb4cf13020e180e2837b3bfb5119d7a3fde
Yang-sakura <147797487+Yang-sakura@users.noreply.github.com>
2024-04-23 14:17:47 +0800

    Update drone.c

--- drone.c
+++ drone.c
@@ -17,44 +17,59 @@ void drone_screen()
 
 void open_file2()
 {
+    int i;
     clrmous(MouseX,MouseY);
-    setfillstyle(SOLID_FILL,LIGHTBLUE);
-    bar(110,50,630,400);
-    printline(110,50,1,104,0,BLUE,5,0);
-    printline(110,395,1,104,0,BLUE,5,0);
-    printline(110,50,1,70,1,BLUE,5,0);
-    printline(625,50,1,70,1,BLUE,5,0);
-    printline(110,95,1,104,0,BLUE,5,0);
-    printline(110,145,1,104,0,BLUE,5,0);
-    printline(110,195,1,104,0,BLUE,5,0);
-    printline(110,245,1,104,0,BLUE,5,0);
-    printline(110,295,1,104,0,BLUE,5,0);
-    printline(110,345,1,104,0,BLUE,5,0);
-    printline(110,395,1,104,0,BLUE,5,0);
-    
-    put_arrow(120,353,DARKGRAY,5,1);
-    put_arrow(540,353,DARKGRAY,5,2);
+    setfillstyle(SOLID_FILL,CYAN);
+    bar(50,50,590,415);
+    setfillstyle(SOLID_FILL,BLUE);
+    for(i=0;i<10;i++)
+    {
+        bar(50,50+i*30,590,50+i*30+5);
+    }
+    bar(50,350,590,355);
+    bar(50,410,590,415);
+    bar(50,50,55,415);
+    bar(585,50,590,415);
+    bar(140,50,145,350);
+    bar(230,50,235,350);
+    bar(320,50,325,350);
+    bar(410,50,415,350);
+    bar(500,50,505,350);
+
+    put_arrow(65,363,DARKGRAY,5,LEFTARROW);  //(65,363,150,403)
+    put_arrow(495,363,DARKGRAY,5,RIGHTARROW); //(495,363,580,403)
+
+    settextstyle(DEFAULT_FONT,HORIZ_DIR,2);
+    outtextxy(65,60,"NAME");
+    outtextxy(180,60,"M");
+    outtextxy(270,60,"N");
+    outtextxy(360,60,"W");
+    outtextxy(450,60,"T");
+    outtextxy(540,60,"P");
+
+    back_button(PAINT);
 }
 
 
 int drone_page(char *username,char *drone_name_now,DRONEINFO *drone)
-{
-    struct ffblk ffblk;   
+{ 
+    struct ffblk ffblk;  
+    int done;
+    int page = 0;
+    int pagemax;
+    int file_number=0;
+    int file_time = 0;
     int flag;
     int num[4];
     int file_flag;
     int filenum[10];
     int mode = 0;
-    int done;
-    int i;
-    int file_number=0;
-    int filetime = 1;
-    int pagemax;
-    int page = 0;
+    int i=0,j=0;
     char string[80] = "c:\\DATA\\";
     char stringnow[80];
     char stringall[80];
-    char drone_list[40][20];
+    // char drone_list[40][20];
+    struct droneinfo drone_list[20];
     char drone_name[20];
     FILE *fp;
     strcat(string,username);
@@ -63,6 +78,7 @@ int drone_page(char *username,char *drone_name_now,DRONEINFO *drone)
     strcpy(stringall,string);
     strcat(stringall,"\\*.*");
 
+    memset(drone_list,0,sizeof(drone_list));
     if(access(string,0)==-1)  //协助创建无人机文件夹
     {
         if(mkdir(string)!=0)
@@ -73,11 +89,38 @@ int drone_page(char *username,char *drone_name_now,DRONEINFO *drone)
         }
     }
 
+    
+
+    done = findfirst(stringall,&ffblk,0);
+    while(!done)
+    {
+        // strcpy(drone_list[i],ffblk.ff_name);
+        strcpy(stringnow,string);
+        strcat(stringnow,"\\");
+        strcat(stringnow,ffblk.ff_name);
+        if((fp=fopen(stringnow,"rb"))!=NULL)
+        {
+            if((fread(&drone_list[i],sizeof(DRONEINFO),1,fp))!=1)
+            {
+                perror("ERROR IN READING");
+            }
+        }
+        else
+        {
+            perror("ERROR IN CREATING!");
+        }
+            fclose(fp);
+        done = findnext(&ffblk);
+        i++;
+        file_number++;
+    }
+
     memset(drone_name,0,sizeof(drone_name));
 
     clrmous(MouseX,MouseY);
     cleardevice();
 
+    put_crop1(10,10,CROP,HEALTHY);
     drone_screen();
     mouseinit();
     while(1)
@@ -103,23 +146,10 @@ int drone_page(char *username,char *drone_name_now,DRONEINFO *drone)
             setfillstyle(SOLID_FILL,WHITE);
             bar(300,60,550,200);
             printbox(300,60,550,200,YELLOW,1,5,5);
-            temp_input(drone_name,330,117,6,33,25,WHITE,4);
+            temp_input(drone_name,330,117,5,33,25,WHITE,4);
             if(strlen(drone_name)!=0)
             {
-                strcpy(stringnow,string);
-                strcat(stringnow,"\\");
-                strcat(stringnow,drone_name);
-                strcat(stringnow,".dat");
-                if((fp=fopen(stringnow,"wb+"))!=NULL)
-                {
-                    fread(&drone,sizeof(DRONEINFO),1,fp);
-                }
-                else
-                {
-                    perror("ERROR IN CREATING!");
-                }
-                fclose(fp);
-                free(drone_list);
+                // free(drone_list);
                 clrmous(MouseX,MouseY);
                 cleardevice();
                 MouseS = 0;
@@ -144,7 +174,7 @@ int drone_page(char *username,char *drone_name_now,DRONEINFO *drone)
         }
         else if(mouse_press(300,260,550,400)==1)
         {
-            mode = 2;
+            mode = 1;
         }
         else if(mouse_press(595,5,630,40)==2)
         {
@@ -192,54 +222,43 @@ int drone_page(char *username,char *drone_name_now,DRONEINFO *drone)
             back_button(RECOVER);
             num[3]=0;
         }
-
         if(mode == 1)
         {
-            setfillstyle(SOLID_FILL,WHITE);
-            bar(80,0,640,480);
+            clrmous(MouseX,MouseY);
+    
 
-            open_file2();
-            put_arrow(120,353,DARKGRAY,5,1);
-            put_arrow(540,353,DARKGRAY,5,2);
-            back_button(PAINT);
-            
-            memset(drone_list,0,sizeof(drone_list));
+            mouseinit();
             while(1)
             {
                 newmouse(&MouseX,&MouseY,&press);
-                if(filetime == 1)
+                if(file_time == 0)
                 {
-                    file_number = 0;
-                    i=0;
-                    done = findfirst(stringall,&ffblk,0);
-                    while(!done)
-                    {
-                        strcpy(drone_list[i],ffblk.ff_name);
-                        string_limitation(drone_list[i],15);
-                        done = findnext(&ffblk);
-                        i++;
-                        file_number++;
-                    }
+                    clrmous(MouseX,MouseY);
+                    cleardevice();
                     open_file2();
                     setcolor(DARKGRAY);
-                    settextstyle(DEFAULT_FONT,HORIZ_DIR,4);
-                    pagemax = file_number/6;
-                    if(file_number%6==0)
+                    settextstyle(DEFAULT_FONT,HORIZ_DIR,2);
+                    pagemax = file_number/9;
+                    if(file_number%9==0)
                     {
                         pagemax -=1;
                     }
-                    if((file_number-6*page)>=0)
+                    if((file_number-9*page)>=0)
                     {
-                        for(i=0;i<6;i++)
+                        clrmous(MouseX,MouseY);
+                        for(i=0;i<9;i++)
                         {
-                            clrmous(MouseX,MouseY);
-                            outtextxy(118,60+50*(i),drone_list[i+page*6]);
+                            outtextxy(60,90+i*30,drone_list[i+page*9].name);
+                            outtextxy(150,90+i*30,drone_list[i+page*9].weight);
+                            outtextxy(240,90+i*30,drone_list[i+page*9].wing);
+                            outtextxy(330,90+i*30,drone_list[i+page*9].weather);
+                            outtextxy(420,90+i*30,drone_list[i+page*9].time);
+                            outtextxy(510,90+i*30,drone_list[i+page*9].power);
                         }
                     }
-                    filetime = 0;
+                    file_time = 1;
                 }
-                
-                if(mouse_press(595,5,630,40)==2)
+                if(mouse_press(65,363,150,403)==2)
                 {
                     if(file_flag!=1)
                     {
@@ -247,18 +266,19 @@ int drone_page(char *username,char *drone_name_now,DRONEINFO *drone)
                         file_flag = 1;
                         filenum[1] = 1;
                         clrmous(MouseX,MouseY);
-                        back_button(LIGHT);
+                        put_arrow(65,363,YELLOW,5,LEFTARROW);  //(65,363,150,403)
                     }
                 }
-                else if(mouse_press(595,5,630,40)==1)
+                else if(mouse_press(65,363,150,403)==1)
                 {
-                    clrmous(MouseX,MouseY);
-                    cleardevice();
-                    mode = 0;
-                    MouseS = 0;
-                    return DRONE;
+                    delay(50);
+                    if(page>=1)
+                    {
+                        page--;
+                        file_time = 0;
+                    }
                 }
-                else if(mouse_press(115,55,625,95)==2)
+                else if(mouse_press(495,363,580,403)==2)
                 {
                     if(file_flag!=2)
                     {
@@ -266,18 +286,19 @@ int drone_page(char *username,char *drone_name_now,DRONEINFO *drone)
                         file_flag = 2;
                         filenum[2] = 1;
                         clrmous(MouseX,MouseY);
-                        setcolor(CYAN);
-                        settextstyle(DEFAULT_FONT,HORIZ_DIR,4);
-                        outtextxy(118,60+50*0,drone_list[0+page*6]);
+                        put_arrow(495,363,YELLOW,5,RIGHTARROW); //(495,363,580,403)
                     }
                 }
-                else if(mouse_press(115,55,625,95)==1)
+                else if(mouse_press(495,363,580,403)==1)
                 {
-                    for(i=0;i<strlen(drone_list[0+page*6])-4;i++)
-                        drone_name_now[i] = drone_list[0+page*6][i];
-                    return DRONE_LIST;
+                    delay(50);
+                    if(page<pagemax)
+                    {
+                        page++;
+                        file_time = 0;
+                    }
                 }
-                else if(mouse_press(115,100,625,145)==2)
+                else if(mouse_press(595,5,630,40)==2)
                 {
                     if(file_flag!=3)
                     {
@@ -285,139 +306,18 @@ int drone_page(char *username,char *drone_name_now,DRONEINFO *drone)
                         file_flag = 3;
                         filenum[3] = 1;
                         clrmous(MouseX,MouseY);
-                        setcolor(CYAN);
-                        settextstyle(DEFAULT_FONT,HORIZ_DIR,4);
-                        outtextxy(118,60+50*1,drone_list[1+page*6]);
+                        back_button(LIGHT);
                     }
                 }
-                else if(mouse_press(115,100,625,145)==1)
+                else if(mouse_press(595,5,630,40)==1)
                 {
-                    for(i=0;i<strlen(drone_list[1+page*6])-4;i++)
-                        drone_name_now[i] = drone_list[1+page*6][i];
-                    return DRONE_LIST;
-                    return DRONE_LIST;
-                }
-                else if(mouse_press(115,150,625,195)==2)
-                {
-                    if(file_flag!=4)
-                    {
-                        MouseS = 1;
-                        file_flag = 4;
-                        filenum[4] = 1;
-                        clrmous(MouseX,MouseY);
-                        setcolor(CYAN);
-                        settextstyle(DEFAULT_FONT,HORIZ_DIR,4);
-                        outtextxy(118,60+50*2,drone_list[2+page*6]);
-                    }
-                }
-                else if(mouse_press(115,150,625,195)==1)
-                {
-                    for(i=0;i<strlen(drone_list[2+page*6])-4;i++)
-                        drone_name_now[i] = drone_list[2+page*6][i];
-                    return DRONE_LIST;
-                }
-                else if(mouse_press(115,200,625,245)==2)
-                {
-                    if(file_flag!=2)
-                    {
-                        MouseS = 1;
-                        file_flag = 5;
-                        filenum[5] = 1;
-                        clrmous(MouseX,MouseY);
-                        setcolor(CYAN);
-                        settextstyle(DEFAULT_FONT,HORIZ_DIR,4);
-                        outtextxy(118,60+50*3,drone_list[3+page*6]);
-                    }
-                }
-                else if(mouse_press(115,200,625,245)==1)
-                {
-                    for(i=0;i<strlen(drone_list[3+page*6])-4;i++)
-                        drone_name_now[i] = drone_list[3+page*6][i];
-                    return DRONE_LIST;
-                }
-                else if(mouse_press(115,250,625,295)==2)
-                {
-                    if(file_flag!=6)
-                    {
-                        MouseS = 1;
-                        file_flag = 6;
-                        filenum[6] = 1;
-                        clrmous(MouseX,MouseY);
-                        setcolor(CYAN);
-                        settextstyle(DEFAULT_FONT,HORIZ_DIR,4);
-                        outtextxy(118,60+50*4,drone_list[4+page*6]);
-                    }
-                }
-                else if(mouse_press(115,250,625,295)==1)
-                {
-                    for(i=0;i<strlen(drone_list[4+page*6])-4;i++)
-                        drone_name_now[i] = drone_list[4+page*6][i];
-                    return DRONE_LIST;
-                }
-                else if(mouse_press(115,300,625,345)==2)
-                {
-                    if(file_flag!=7)
-                    {
-                        MouseS = 1;
-                        file_flag = 7;
-                        filenum[7] = 1;
-                        clrmous(MouseX,MouseY);
-                        setcolor(CYAN);
-                        settextstyle(DEFAULT_FONT,HORIZ_DIR,4);
-                        outtextxy(118,60+50*5,drone_list[5+page*6]);
-                    }
-                }
-                else if(mouse_press(115,300,625,345)==1)
-                {
-                    for(i=0;i<strlen(drone_list[5+page*6])-4;i++)
-                        drone_name_now[i] = drone_list[5+page*6][i];
-                    return DRONE_LIST;
-                }
-                else if(mouse_press(120,353,200,393)==2)
-                {
-                    if(file_flag!=8)
-                    {
-                        MouseS = 1;
-                        file_flag = 8;
-                        filenum[8] = 1;
-                        clrmous(MouseX,MouseY);
-                        put_arrow(120,353,CYAN,5,1);
-                    }
-                }
-                else if(mouse_press(120,353,200,393)==1)
-                {
-                    delay(50);
-                    if(page>=1)
-                    {
-                        page--;
-                        filetime = 1;
-                    }
-                }
-                else if(mouse_press(540,353,620,393)==2)
-                {
-                    if(file_flag!=9)
-                    {
-                        MouseS = 1;
-                        file_flag = 9;
-                        filenum[9] = 1;
-                        clrmous(MouseX,MouseY);
-                        put_arrow(540,353,CYAN,5,2);
-                    }
-                }
-                else if(mouse_press(540,353,620,393)==1)
-                {
-                    delay(50);
-                    if(page<pagemax)
-                    {
-                        page++;
-                        filetime = 1;
-                    }
+                    return DRONE;
                 }
                 else
                 {
                     if(file_flag!=0)
                     {
-                        MouseS = 1;
+                        MouseS = 0;
                         file_flag = 0;
                     }
                 }
@@ -425,71 +325,24 @@ int drone_page(char *username,char *drone_name_now,DRONEINFO *drone)
                 if(file_flag!=1&&filenum[1]==1)
                 {
                     clrmous(MouseX,MouseY);
-                    back_button(RECOVER);
+                    put_arrow(65,363,DARKGRAY,5,LEFTARROW);  
                     filenum[1] = 0;
                 }
                 else if(file_flag!=2&&filenum[2]==1)
                 {
                     clrmous(MouseX,MouseY);
-                    setcolor(DARKGRAY);
-                    settextstyle(DEFAULT_FONT,HORIZ_DIR,4);
-                    outtextxy(118,60+50*0,drone_list[0+page*6]);
+                    put_arrow(495,363,DARKGRAY,5,RIGHTARROW);
                     filenum[2] = 0;
                 }
                 else if(file_flag!=3&&filenum[3]==1)
                 {
                     clrmous(MouseX,MouseY);
-                    setcolor(DARKGRAY);
-                    settextstyle(DEFAULT_FONT,HORIZ_DIR,4);
-                    outtextxy(118,60+50*1,drone_list[1+page*6]);
+                    back_button(RECOVER);
                     filenum[3] = 0;
                 }
-                else if(file_flag!=4&&filenum[4]==1)
-                {
-                    clrmous(MouseX,MouseY);
-                    setcolor(DARKGRAY);
-                    settextstyle(DEFAULT_FONT,HORIZ_DIR,4);
-                    outtextxy(118,60+50*2,drone_list[2+page*6]);
-                    filenum[4] = 0;
-                }
-                else if(file_flag!=5&&filenum[5]==1)
-                {
-                    clrmous(MouseX,MouseY);
-                    setcolor(DARKGRAY);
-                    settextstyle(DEFAULT_FONT,HORIZ_DIR,4);
-                    outtextxy(118,60+50*3,drone_list[3+page*6]);
-                    filenum[5] = 0;
-                }
-                else if(file_flag!=6&&filenum[6]==1)
-                {
-                    clrmous(MouseX,MouseY);
-                    setcolor(DARKGRAY);
-                    settextstyle(DEFAULT_FONT,HORIZ_DIR,4);
-                    outtextxy(118,60+50*4,drone_list[4+page*6]);
-                    filenum[6] = 0;
-                }
-                else if(file_flag!=7&&filenum[7]==1)
-                {
-                    clrmous(MouseX,MouseY);
-                    setcolor(DARKGRAY);
-                    settextstyle(DEFAULT_FONT,HORIZ_DIR,4);
-                    outtextxy(118,60+50*5,drone_list[5+page*6]);
-                    filenum[7] = 0;
-                }
-                else if(file_flag!=8&&filenum[8]==1)
-                {
-                    clrmous(MouseX,MouseY);
-                    put_arrow(120,353,DARKGRAY,5,1);
-                    filenum[8] = 0;
-                }
-                else if(file_flag!=9&&filenum[9]==1)
-                {
-                    clrmous(MouseX,MouseY);
-                    put_arrow(540,353,DARKGRAY,5,2);
-                    filenum[9] = 0;
-                }
-
             }
+
         }
+
     }
 }
