890716fff77b2da7c3c081d4a12a76f49ec3c135
xuznotfound <141018245+xuznotfound@users.noreply.github.com>
2024-04-25 17:03:55 +0800

    Update flyfunc.c finally!!!

--- flyfunc.c
+++ flyfunc.c
@@ -124,14 +124,19 @@ void fly_detect(int record[21][26] , Point start )
 }
 void fly_spray(int record[21][26], int n )
 {
-    int i,j,k ,x,y,count,flying,size,nx,ny,next_target = 0,now_x,now_y,closest_x,closest_y;
-    double dis = 99999.99 , temp ,max_double = 99999.99;
+    int i,j,k ,t,x,y,count,flying,size,nx,ny,next_target = 0,now_x,now_y,closest_x,closest_y;
+    double dis = 99999.99 , temp ,max_double = 99999.99 ;
+    double distance ,dx,dy,delta_x , delta_y ;
+    double threshold = 10.00 ;
     int flag[21][26];    //此处内存可以通过+100节省，但是记住要还原record的值
     Point route[5][50];
     int num[5],fly[5],drone_flag_n[5];
     double step[5] , step_x[5] , step_y[5], x_n[5] , y_n[5];
     void *drone_buffer0 ,*drone_buffer1 , *drone_buffer2, *drone_buffer3 ;
     char temp_out[20];
+    // Point out[1000000];
+    // t=0 ;
+    // memset(out,0,sizeof(out));
 
     memset(route,0,sizeof(route));
     memset(flag,0,sizeof(flag));
@@ -149,6 +154,7 @@ void fly_spray(int record[21][26], int n )
     setcolor(DARKGRAY);
     settextstyle(DEFAULT_FONT,HORIZ_DIR,1);
 
+    //存储起点
     for(i=0;i<21;i++)//y
     {
         for(j=0;j<26;j++)//x
@@ -175,23 +181,13 @@ void fly_spray(int record[21][26], int n )
             }
         }
     }
-    // setfillstyle(SOLID_FILL,WHITE);
-    // bar(480,20,530,40);
-    // itoa(count,temp_out,10);
-    // outtextxy(500,30,temp_out);
+    setfillstyle(SOLID_FILL,WHITE);
+    bar(480,20,530,40);
+    itoa(count,temp_out,10);
+    outtextxy(500,30,temp_out);
     if(count==0) return ;
 
-    // for(k=0;k<4;k++)
-    // {
-    //     for(i=0;i<num[k];i++) {
-    //         itoa(route[k][i].x,temp_out,10);
-    //         outtextxy(15+100*k,300+i*10,temp_out);
-    //         itoa(route[k][i].y,temp_out,10);
-    //         outtextxy(75+100*k,300+i*10,temp_out);
-    //     }
-    // }
-    // return ;
-
+    //计算病虫
     while(count > 0)
     {
         for(k=0 ; k <= n-1 ; k++)
@@ -228,16 +224,6 @@ void fly_spray(int record[21][26], int n )
             if(count==0) break ;
         }
     }
-    // setcolor(DARKGRAY);
-    // settextstyle(DEFAULT_FONT,HORIZ_DIR,1);
-    
-    // for(i=0;i<num[0];i++) {
-    //     itoa(route[0][i].x,temp_out,10);
-    //     outtextxy(15,300+i*10,temp_out);
-    //     itoa(route[0][i].y,temp_out,10);
-    //     outtextxy(70,300+i*10,temp_out);
-    // }
-
     // route[0][0].x=200 ;route[0][0].y = 100 ;
     // route[0][1].x=200 ;route[0][1].y = 150 ;
     // route[0][2].x=200 ;route[0][2].y = 200 ;
@@ -253,13 +239,15 @@ void fly_spray(int record[21][26], int n )
     // route[2][2].x=400 ;route[2][2].y = 200 ;
     // route[2][3].x=400 ;route[2][3].y = 250 ;
     // num[2] = 4 ;
-    
+
+    //存储结尾
     for(k = 0 ; k <= n-1 ; k++)
     {
         num[k]++ ;
         route[k][num[k]-1].x  = route[k][0].x;
         route[k][num[k]-1].y  = route[k][0].y;
     }
+
     for(k=0;k<4;k++)
     {
         for(i=0;i<num[k];i++) {
@@ -269,44 +257,58 @@ void fly_spray(int record[21][26], int n )
             outtextxy(75+100*k,300+i*10,temp_out);
         }
     }
+    
     flying = 0 ;
     for( k = 0 ; k < n ; k++)
     {
-        if( num[k]!=2 ) flying = 1 ;
+        if( num[k] != 2 ) flying = 1 ;
     }
-    if(flying==0 ) return ;
+    if( flying == 0 ) return ;
 
-    for( k = 0 ; k < n ; k++)//step初始化
+    for( k = 0 ; k < n ; k++)//step初始化 0-1
     {
         if(num[k]==2) continue ;
         fly[k] = 1 ;
         step[k] = max(abs(route[k][1].x - route[k][0].x), abs(route[k][1].y - route[k][0].y));
         if (step[k] < 1) step[k] = 1;
-        step[k] /= 1.5; // 速度修改
+        // step[k] /= 1.5; // 速度修改
         step_x[k] = (route[k][1].x - route[k][0].x) / step[k];
         step_y[k] = (route[k][1].y - route[k][0].y) / step[k];
+
         x_n[k] = route[k][0].x;
         y_n[k] = route[k][0].y;
     }
-    flying = 1 ;
+    
+    
     setfillstyle(SOLID_FILL,RED);
     bar(30,450,40,460);
+
+    // for(k=0;k<4;k++)
+    // {
+
+    //         itoa(num[k],temp_out,10);
+    //         outtextxy(15+100*k,300,temp_out);
+    //         itoa(fly[k],temp_out,10);
+    //         outtextxy(15+100*k,400,temp_out);
+    // }
+    // return ;
+    flying = 1 ;
     while(flying)
     {
         flying = 0 ;
         for( k = 0 ; k < n ; k++) 
         {
             if(num[k]==2) continue ;
-            if (fly[k] < num[k]) //fly[k]为当前飞机已经飞过的点，fly[k]+1为下一个要飞到的点
+            if (fly[k] < num[k] ) //fly[k]为当前飞机已经飞过的点，fly[k]+1为下一个要飞到的点
             {
                 flying = 1 ;
             }
         }
+        if(flying == 0 ) break ;
         
         for( k = 0 ; k < n ; k++) 
         {
-            if(num[k]==2) continue ;
-            if (fly[k] == num[k]) continue ;//已经飞到
+            if(num[k]==2|| fly[k] >= num[k] ) continue ;
             nx = (int)x_n[k] ;
             ny = (int)y_n[k] ;
             size = imagesize(nx-10, ny-10, nx + 10, ny + 10);//save_bk_drone((int)x,(int)y);
@@ -345,12 +347,11 @@ void fly_spray(int record[21][26], int n )
                 drone_flag_n[k] = 1 ;
             }
         }
-        delay(150);
+        delay(100);
         
         for( k = 0 ; k < n ; k++) 
         {
-            if(num[k]==2) continue ;
-            if (fly[k] == num[k]) continue ;
+            if(num[k]==2 || fly[k] >= num[k] ) continue ;
             nx = (int)x_n[k] ;
             ny = (int)y_n[k] ;
             if(drone_flag_n[k] == 1 ) {
@@ -375,26 +376,38 @@ void fly_spray(int record[21][26], int n )
                 drone_flag_n[k] = 0;
                 setwritemode(COPY_PUT);
             }
-            
         }
-        
-        for( k = 0 ; k < n ; k++) 
+        flying =  1 ;
+        for( k = 0 ; k < n ; k++) //从fly[k]飞到fly[k]+1的点的过程
         {
-            if(num[k]==2) continue ;
+            if(num[k]==2 ) continue ;
             x_n[k] += step_x[k] ;
             y_n[k] += step_y[k] ;
-            next_target = fly[k]+1 ;
-            if( (x_n[k]== route[k][next_target].x && y_n[k]== route[k][next_target].y ) && fly[k]<num[k])//飞机已经飞到下一个点，更新step
+            if(fly[k] >= num[k] ) {
+                step_x[k] = 0;
+                step_y[k] = 0;
+                continue ;
+            }
+            next_target = fly[k] +1 ;//route[k][next_target-1]为target这个点实际储存的点的位置
+            if((abs(x_n[k]-route[k][next_target-1].x)<=threshold) && (abs(y_n[k]-route[k][next_target-1].y )<=threshold ))//飞机已经飞到下一个点，更新step
             {
-                fly[k] += 1 ;
-                next_target = fly[k]+1;
-                step[k] = max(abs(route[k][next_target].x - (int)x_n[k]), abs(route[k][next_target].y - (int)y_n[k]));
-                if (step[k] < 1) step[k] = 1;
-                step[k] /= 1.5; // 速度修改
-                step_x[k] = (route[k][next_target].x - (int)x_n[k]) / step[k];
-                step_y[k] = (route[k][next_target].y - (int)y_n[k]) / step[k];
+                fly[k] ++ ;
+                if( fly[k] < num[k] )
+                {
+                    next_target = fly[k] +1 ;
+                    step[k] = max(abs(route[k][next_target-1].x - (int)x_n[k]), abs(route[k][next_target-1].y - (int)y_n[k]));
+                    if (step[k] < 1) step[k] = 1;
+                    // step[k] /= 1.5; // 速度修改
+                    step_x[k] = (route[k][next_target-1].x - (int)x_n[k]) / step[k];
+                    step_y[k] = (route[k][next_target-1].y - (int)y_n[k]) / step[k];
+                }
+                else {
+                    step_x[k] = 0;
+                    step_y[k] = 0;
+                }
             }
         }
         
     }
+    return ;
 }
