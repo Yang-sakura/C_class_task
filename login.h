69c08fa85ee586060e7b32963f6359890fd2293a
Yang-sakura <147797487+Yang-sakura@users.noreply.github.com>
2024-04-26 18:09:59 +0800

    Update login.h

--- login.h
+++ login.h
@@ -1,13 +1,12 @@
-#ifndef _LOGIN_H_
-#define _LOGIN_H_
 
 #include "public.h"
 
-void login_bkpaint(void);//画登录页面背景
-void signup_button_recover(void);
-void signup_button_light(void);
+void login_bkpaint(int language);//鐢荤櫥褰曢〉闈㈣儗鏅?
+void signup_button_recover(int language);
+void signup_button_light(int language);
+void put_flower(int x,int y,int pix,int COLOR);
 void ok_button_light(void);
 void ok_button_recover(void);
-int login_page(INFO *temp);
+int login_page(INFO *temp,int language);
 
 #endif
