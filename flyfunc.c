a29517c9938a024e2c464250694c3d9011e305a1
xuznotfound <141018245+xuznotfound@users.noreply.github.com>
2024-04-26 12:00:32 +0800

    Update flyfunc.c

--- flyfunc.c
+++ flyfunc.c
@@ -125,18 +125,15 @@ void fly_detect(int record[21][26] , Point start )
 void fly_spray(int record[21][26], int n )
 {
     int i,j,k ,t,x,y,count,flying,size,nx,ny,next_target = 0,now_x,now_y,closest_x,closest_y;
-    double dis = 99999.99 , temp ,max_double = 99999.99 ;
+    double dis = 99999.99 , temp , max_double = 99999.99 ;
     double distance ,dx,dy,delta_x , delta_y ;
-    double threshold = 10.00 ;
+    double threshold = 5.00 ;
     int flag[21][26];    //此处内存可以通过+100节省，但是记住要还原record的值
     Point route[5][50];
-    int num[5],fly[5],drone_flag_n[5];
+    int num[5] , fly[5] , drone_flag_n[5] , drone_if_draw[5] ;
     double step[5] , step_x[5] , step_y[5], x_n[5] , y_n[5];
     void *drone_buffer0 ,*drone_buffer1 , *drone_buffer2, *drone_buffer3 ;
     char temp_out[20];
-    // Point out[1000000];
-    // t=0 ;
-    // memset(out,0,sizeof(out));
 
     memset(route,0,sizeof(route));
     memset(flag,0,sizeof(flag));
@@ -147,6 +144,7 @@ void fly_spray(int record[21][26], int n )
     memset(step_y,0,sizeof(step_y));
     memset(x_n,0,sizeof(x_n));
     memset(y_n,0,sizeof(y_n));
+    memset(drone_if_draw,0,sizeof(drone_if_draw));
 
     k = 0 ;
     count = 0 ;
@@ -171,13 +169,10 @@ void fly_spray(int record[21][26], int n )
             }
             if((record[i][j] % 10 ) != 0 && record[i][j] >= 10) {
                 count ++ ;//多少株植物得病了
-                setlinestyle(SOLID_LINE,0,NORM_WIDTH);
-                setcolor(RED);
-                line(x,y+20,x+20,y+20);
-                // itoa(i,temp_out,10);
-                // outtextxy(15+j*10,350+i*10,temp_out);
-                // itoa(j,temp_out,10);
-                // outtextxy(30+j*10,350+i*10,temp_out);
+                record[i][j]-- ;//喷洒了一次
+                // setlinestyle(SOLID_LINE,0,NORM_WIDTH);
+                // setcolor(RED);
+                // line(x,y+20,x+20,y+20);
             }
         }
     }
@@ -205,7 +200,7 @@ void fly_spray(int record[21][26], int n )
                     if(record[i][j]<10 || record[i][j]>99 || record[i][j]%10 == 0) continue ;
                     x = 110 + j*20 ;
                     y = 450 - i*20 ;
-                    temp = sqrt( (x-now_x)*(x-now_x) + (y-now_y)*(y-now_y) ) ;
+                    temp = sqrt(fabs( (x-now_x)*(x-now_x) + (y-now_y)*(y-now_y) )) ;
                     if(temp < dis) {
                         dis = temp ;
                         closest_x = x ;
@@ -224,21 +219,6 @@ void fly_spray(int record[21][26], int n )
             if(count==0) break ;
         }
     }
-    // route[0][0].x=200 ;route[0][0].y = 100 ;
-    // route[0][1].x=200 ;route[0][1].y = 150 ;
-    // route[0][2].x=200 ;route[0][2].y = 200 ;
-    // route[0][3].x=200 ;route[0][3].y = 250 ;
-    // num[0] = 4 ;
-    // route[1][0].x=300 ;route[1][0].y = 100 ;
-    // route[1][1].x=300 ;route[1][1].y = 150 ;
-    // route[1][2].x=300 ;route[1][2].y = 200 ;
-    // route[1][3].x=300 ;route[1][3].y = 250 ;
-    // num[1] = 4 ;
-    // route[2][0].x=400 ;route[2][0].y = 100 ;
-    // route[2][1].x=400 ;route[2][1].y = 150 ;
-    // route[2][2].x=400 ;route[2][2].y = 200 ;
-    // route[2][3].x=400 ;route[2][3].y = 250 ;
-    // num[2] = 4 ;
 
     //存储结尾
     for(k = 0 ; k <= n-1 ; k++)
@@ -248,15 +228,15 @@ void fly_spray(int record[21][26], int n )
         route[k][num[k]-1].y  = route[k][0].y;
     }
 
-    for(k=0;k<4;k++)
-    {
-        for(i=0;i<num[k];i++) {
-            itoa(route[k][i].x,temp_out,10);
-            outtextxy(15+100*k,300+i*10,temp_out);
-            itoa(route[k][i].y,temp_out,10);
-            outtextxy(75+100*k,300+i*10,temp_out);
-        }
-    }
+    // for(k=0;k<4;k++)
+    // {
+    //     for(i=0;i<num[k];i++) {
+    //         itoa(route[k][i].x,temp_out,10);
+    //         outtextxy(15+100*k,300+i*10,temp_out);
+    //         itoa(route[k][i].y,temp_out,10);
+    //         outtextxy(75+100*k,300+i*10,temp_out);
+    //     }
+    // }
     
     flying = 0 ;
     for( k = 0 ; k < n ; k++)
@@ -271,7 +251,7 @@ void fly_spray(int record[21][26], int n )
         fly[k] = 1 ;
         step[k] = max(abs(route[k][1].x - route[k][0].x), abs(route[k][1].y - route[k][0].y));
         if (step[k] < 1) step[k] = 1;
-        // step[k] /= 1.5; // 速度修改
+        step[k] /= 1.5; // 速度修改
         step_x[k] = (route[k][1].x - route[k][0].x) / step[k];
         step_y[k] = (route[k][1].y - route[k][0].y) / step[k];
 
@@ -279,19 +259,6 @@ void fly_spray(int record[21][26], int n )
         y_n[k] = route[k][0].y;
     }
     
-    
-    setfillstyle(SOLID_FILL,RED);
-    bar(30,450,40,460);
-
-    // for(k=0;k<4;k++)
-    // {
-
-    //         itoa(num[k],temp_out,10);
-    //         outtextxy(15+100*k,300,temp_out);
-    //         itoa(fly[k],temp_out,10);
-    //         outtextxy(15+100*k,400,temp_out);
-    // }
-    // return ;
     flying = 1 ;
     while(flying)
     {
@@ -305,13 +272,17 @@ void fly_spray(int record[21][26], int n )
             }
         }
         if(flying == 0 ) break ;
-        
+        memset(drone_if_draw,0,sizeof(drone_if_draw));
         for( k = 0 ; k < n ; k++) 
         {
-            if(num[k]==2|| fly[k] >= num[k] ) continue ;
+            if( num[k]==2 || fly[k] >= num[k] ) {
+                drone_if_draw[k] = 0 ;
+                continue ;
+            }
             nx = (int)x_n[k] ;
             ny = (int)y_n[k] ;
             size = imagesize(nx-10, ny-10, nx + 10, ny + 10);//save_bk_drone((int)x,(int)y);
+            drone_if_draw[k] = 1 ;
             if(k==0) {
                 drone_buffer0 = malloc(size);
                 if (drone_buffer0 != NULL)
@@ -340,10 +311,14 @@ void fly_spray(int record[21][26], int n )
                 else
                     printf("Error");
             }
-            if(drone_flag_n[k] == 0 ) //draw_drone
+            
+        }
+        for( k = 0 ; k < n ; k++) 
+        {
+            if( drone_flag_n[k] == 0 && drone_if_draw[k] == 1 ) //draw_drone
             {
                 setwritemode(COPY_PUT);
-                put_drone1(nx, ny , 2);
+                put_drone1((int)x_n[k], (int)y_n[k], 2);
                 drone_flag_n[k] = 1 ;
             }
         }
@@ -397,7 +372,7 @@ void fly_spray(int record[21][26], int n )
                     next_target = fly[k] +1 ;
                     step[k] = max(abs(route[k][next_target-1].x - (int)x_n[k]), abs(route[k][next_target-1].y - (int)y_n[k]));
                     if (step[k] < 1) step[k] = 1;
-                    // step[k] /= 1.5; // 速度修改
+                    step[k] /= 1.5; // 速度修改
                     step_x[k] = (route[k][next_target-1].x - (int)x_n[k]) / step[k];
                     step_y[k] = (route[k][next_target-1].y - (int)y_n[k]) / step[k];
                 }
@@ -410,4 +385,222 @@ void fly_spray(int record[21][26], int n )
         
     }
     return ;
+}
+
+double dis_a_to_b(Point a ,Point b )
+{
+    return sqrt(fabs( (a.x - b.x)*(a.x - b.x) + (a.y - b.y )*(a.y - b.y ) ));
+}
+
+double relative_position(Point A ,Point B ,Point C)// 判断点C相对于直线AB的位置，返回正值表示一侧，负值表示另一侧，0表示在线上
+{
+    return (B.x - A.x) * (C.y - A.y) - (B.y - A.y) * (C.x - A.x);
+}
+double projection(Point A, Point B, Point C) 
+{
+    double ABx,ABy,ACx,ACy,dotProduct,lenAB ;
+    ABx = B.x - A.x;
+    ABy = B.y - A.y;
+    ACx = C.x - A.x;
+    ACy = C.y - A.y;
+    
+    dotProduct = ABx * ACx + ABy * ACy;// 计算向量点乘，得到投影长度
+    
+    lenAB = sqrt(fabs(ABx * ABx + ABy * ABy));// AB向量长度
+    
+    return dotProduct / lenAB;// 投影的长度
+}
+void fly_one_round(int record[21][26] , Point A)
+{
+    Point points[100]  ,B , route[100] ,T;
+    int x,y,i,j , k=0 , t=0 , positive = 0 , negative = 0 ,B_index;
+    int flag[100];
+    double dist , dis_max = 0 ,  proj[100] , temp;
+    double pos[100] ;
+    int now_index = -1;
+    char temp_out[20];
+
+    memset(points,0,sizeof(points));
+    memset(route,0,sizeof(route));
+    memset(flag,0,sizeof(flag));
+
+    setfillstyle(SOLID_FILL,RED);
+    bar(50,300,60,310);
+
+    settextstyle(DEFAULT_FONT,HORIZ_DIR,1);
+    setcolor(DARKGRAY);
+    itoa(A.x,temp_out,10);
+    outtextxy(500,10,temp_out);
+    itoa(A.y,temp_out,10);
+    outtextxy(500,20,temp_out);
+
+    k = 0 ;
+    for(i=0;i<21;i++)//y
+    {
+        for(j=0;j<26;j++)//x
+        {
+            x = 110 + j*20 ;
+            y = 450 - i*20 ;
+            if( record[i][j] >= 10 && ( record[i][j]%10 != 0 ) ) //sick
+            {
+                k++ ;
+                points[k].x = x;
+                points[k].y = y ;
+            }
+        }
+    }
+
+    settextstyle(DEFAULT_FONT,HORIZ_DIR,1);
+    setcolor(DARKGRAY);
+    for(i=1;i<=k;i++)//points: 1 - k
+    {
+        itoa(points[i].x,temp_out,10);
+        outtextxy(15,300+i*10,temp_out);
+        itoa(points[i].y,temp_out,10);
+        outtextxy(75,300+i*10,temp_out);
+    }
+    setfillstyle(SOLID_FILL,CYAN);
+    bar(50,310,60,320);
+
+    itoa(k,temp_out,10);
+    outtextxy(500,5,temp_out);
+
+    for(i=1 ; i <= k ; i++)
+    {
+        dist = dis_a_to_b(A,points[i]);
+        if(dist > dis_max ) {
+            dis_max = dist ;
+            B = points[i];
+            B_index = i ;
+        }
+    }
+
+    itoa(B.x,temp_out,10);
+    outtextxy(500,35,temp_out);
+    itoa(B.y,temp_out,10);
+    outtextxy(500,45,temp_out);
+    itoa(B_index,temp_out,10);
+    outtextxy(500,55,temp_out);
+    setfillstyle(SOLID_FILL,GREEN);
+    bar(50,320,60,330);
+
+    positive = negative = 0 ;
+    for(i=1 ;i <= k ;i++)
+    {
+        if(i == B_index ) {
+            pos[B_index] = 0 ;
+        }
+        else {
+            pos[i] = relative_position(A,B,points[i]);
+        }
+        
+        proj[i] = projection(A,B,points[i]);
+        if(pos[i] >= 0 ) {
+            pos[i] = 1 ;
+            positive ++ ;
+        }
+        else {
+            pos[i] = -1 ;
+            negative ++ ;
+        }
+    }
+    settextstyle(DEFAULT_FONT,HORIZ_DIR,1);
+    setcolor(DARKGRAY);
+    itoa(positive,temp_out,10);
+    outtextxy(530,35,temp_out);
+    itoa(negative,temp_out,10);
+    outtextxy(530,45,temp_out);
+
+    settextstyle(DEFAULT_FONT,HORIZ_DIR,1);
+    setcolor(DARKGRAY);
+    for(i=1;i<=k;i++)
+    {
+        itoa(pos[i],temp_out,10);
+        outtextxy(5,400+i*10,temp_out);
+        itoa(proj[i],temp_out,10);
+        outtextxy(120,400+i*10,temp_out);
+    }
+    setfillstyle(SOLID_FILL,BLUE);
+    bar(50,330,60,340);
+
+    temp = 99999.99 ;
+    t = 0 ;
+    route[t].x = A.x ;
+    route[t].y = A.y ;
+    flag[0] = 1  ;
+    now_index = 0;
+    while( positive )
+    {
+        temp = 99999.99 ;
+        for(i=1;i<=k;i++)
+        {
+            if(pos[i] < 0 ) continue ;
+            if(flag[i]) continue ;
+            if(proj[i] < temp) {
+                temp = proj[i] ;
+                T.x = points[i].x;
+                T.y = points[i].y;
+                now_index = i;
+            }
+        }
+        flag[now_index] = 1;
+        t++ ;
+        route[t].x = T.x ;
+        route[t].y = T.y ;
+        positive -- ;
+    }
+    itoa(t,temp_out,10);
+    outtextxy(540,35,temp_out);
+    itoa(positive,temp_out,10);
+    outtextxy(540,45,temp_out);
+    temp = 0 ;
+
+    while( negative )
+    {
+        temp = 0 ;
+        for(i=1;i<=k;i++)
+        {
+            if(pos[i] > 0 ) continue ;
+            if(flag[i]) continue ;
+            if(proj[i] > temp) {
+                temp = proj[i] ; 
+                T.x = points[i].x;
+                T.y = points[i].y;
+                now_index = i;
+            }
+        }
+        flag[now_index] = 1;
+        t++ ;
+        route[t].x = T.x ;
+        route[t].y = T.y ;
+        negative -- ;
+    }
+    itoa(t,temp_out,10);
+    outtextxy(540,45,temp_out);
+    // settextstyle(DEFAULT_FONT,HORIZ_DIR,1);
+    // setcolor(DARKGRAY);
+    // itoa(positive,temp_out,10);
+    // outtextxy(530,35,temp_out);
+    // itoa(negative,temp_out,10);
+    // outtextxy(530,45,temp_out);
+    for(i=0;i<=k;i++)
+    {
+        itoa(route[i].x,temp_out,10);
+        outtextxy(25,400+i*10,temp_out);
+        itoa(route[i].y,temp_out,10);
+        outtextxy(75,400+i*10,temp_out);
+    }
+    setfillstyle(SOLID_FILL,YELLOW);
+    bar(50,340,60,350);
+
+    t++ ;
+    route[t].x = A.x ;
+    route[t].y = A.y ;
+    for(i=0;i<t;i++)
+    {
+        setfillstyle(SOLID_FILL,LIGHTBLUE);
+        bar(50,350,60,360);
+        move_drone1(record ,route[i].x+10,route[i].y+10,route[i+1].x+10,route[i+1].y+10) ;
+    }
+
 }
