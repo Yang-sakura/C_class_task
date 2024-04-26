452d79e5f0dbecdc2979e0207735a96d921fc929
Yang-sakura <147797487+Yang-sakura@users.noreply.github.com>
2024-04-26 18:46:25 +0800

    Update field.h

--- field.h
+++ field.h
@@ -1,10 +1,10 @@
 #ifndef _FIELD_H_
 #define _FIELD_H_
 
-#include "fieldfunc.h"
+#include "fief.h"
 #include "public.h"
 
-void field_screen();
-int field_page(INFO* temp);
+void field_screen( int record[21][26] ,char *now_field,int language);
+int field_page(INFO* temp,char* now_field,int language);
 
 #endif
